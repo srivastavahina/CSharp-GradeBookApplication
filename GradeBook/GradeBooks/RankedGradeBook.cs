e93456770716a584b57f207313a3c61b59de8f0c
srivastava.hina@gmail.com <srivastava.hina@gmail.com>
2020-09-20 17:44:42 +0530

    3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,12 @@ namespace GradeBook.GradeBooks
             {
             Type = Enums.GradeBookType.Ranked;
             }
+        public override char GetLetterGrade(double averageGrade)
+
+        {
+            return 'F';
+        
+        
+        }
     }
 }
