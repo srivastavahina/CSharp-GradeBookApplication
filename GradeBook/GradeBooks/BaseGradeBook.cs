d1caa7f1cdbadcb4502fec79f184d2d2ad5a862f
srivastava.hina@gmail.com <srivastava.hina@gmail.com>
2020-09-20 09:10:16 +0530

    3.2

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,6 +16,7 @@ namespace GradeBook.GradeBooks
         public BaseGradeBook baseGradebook { get; set; }
         public List<Student> Students { get; set; }
 
+        public GradeBookType Type { get; set; }
         public BaseGradeBook(string name)
         {
             Name = name;
